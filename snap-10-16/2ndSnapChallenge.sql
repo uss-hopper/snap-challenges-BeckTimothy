36f4d9c20aa90eaa0c5dd66d335c5580e6c450a0
Timothy Beck <johndaveleet@gmail.com>
2019-10-16 09:16:26 -0600

    refactored select statement

--- snap-10-16/2ndSnapChallenge.sql
+++ snap-10-16/2ndSnapChallenge.sql
@@ -1,1 +1,5 @@
-SELECT tweetContent, profileAtHandle FROM tweet JOIN like WHERE tweetId === "0536faef082b454e9d444cdbe7887d7a";
+SELECT tweet.tweetContent, profile.profileAtHandle
+FROM like
+INNER JOIN like ON like.likeTweetId = tweet.tweetId
+INNER JOIN profile ON like.likeProfileId = profile.profileId
+WHERE tweet.tweetId = UNHEX("0536faef082b454e9d444cdbe7887d7a");
