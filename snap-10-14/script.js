dde49acb388208f5db7df6ac9cd02088bbd54a4f
Timothy Beck <johndaveleet@gmail.com>
2019-10-21 08:30:37 -0600

    created class something

--- snap-10-14/script.js
+++ snap-10-14/script.js
@@ -1,18 +1,23 @@
+let weight = document.getElementById(unitValue);
+let unit = document.getElementById(unitType);
+
+
 function convertToGrams(weight, unit) {
-	if(unit === 'lbs' || unit ==='lb'){
+	if(unit.toLowerCase() === 'lbs' || unit.toLowerCase() ==='lb'){
 		return weight * 453.592;
-	}else if(unit === 'oz') {
+	}else if(unit.toLowerCase() === 'oz') {
 		return weight * 28.3495;
-	}else if(unit === 'kg') {
+	}else if(unit.toLowerCase() === 'kg') {
 		return weight * 1000;
-	}else if(unit === 'g') {
+	}else if(unit.toLowerCase() === 'g') {
 		return weight;
-	}else if(unit === 'mg') {
+	}else if(unit.toLowerCase() === 'mg') {
 		return weight / 1000;
 	}else{return 'Please enter a valid unit of measurement.'}
 }
 
 function sumOfFactors(num) {
+	num = Math.abs(num);
 	let val = 0;
 	for(i = num; i >= 0; i--){
 		if(num % i === 0) {
@@ -21,3 +26,33 @@ function sumOfFactors(num) {
 	}
 	return val;
 }
+//store factor button
+let factorButton = document.getElementById(funcButton);
+//lister for factor button
+factorButton.addEventListener("click", factorButtonClick);
+//store factor return span
+let factorReturn = document.getElementById(factReturn);
+//Callback function that changes those elements
+let factorButtonClick = function() {
+	let num = document.getElementById(numToFactor);
+	let inputHTML = sumOfFactors(num.value);
+	return factReturn.innerHTML = `<p>Sum of Factors: ${inputHTML}</p>`;
+};
+
+/*
+//Store Elements we want to interact with
+//store conversion button
+let convertButton = document.getElementById(convButton);
+
+//fields we want to change
+let convertReturn = document.getElementById(convReturn);
+
+
+let convertButtonClick = function() {
+	return convertReturn.textContent = `Equal to ${convertToGrams(weight.value, unit.value)} grams`;
+};
+//Event listeners for elements
+//lister for unit value field
+convertButton.addEventListener("click", convertButtonClick);
+
+ */
---
+++ snap-10-21/Something.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ *Creating class Something that creates some kind of objects
+ *
+ * @author Timothy Beck <dev@timothybeck.com>
+ */
+class Something {
+	/**
+	 * id for this thing
+	 * @var int $somthingId
+	 */
+	private $somethingId;
+	/**
+	 * name for this thing
+	 * @var string $somthingName
+	 */
+	private $somethingName;
+	/**
+	 * getter for somethingId
+	 *
+	 * @return string value of $somethingId
+	 */
+	public function getSomethingId() {
+		return($this->somethingId);
+	}
+	/**
+	 * getter for somethingName
+	 *
+	 * @return string value of $somethingName
+	 */
+	public function getSomethingName() {
+		return($this->somethingName);
+	}
+	/**
+	 * setter for somethingId
+	 *
+	 * @param int $newSomethingId
+	 * @throws \InvalidArgumentException if not an integer
+	 */
+	public function setSomethingId($newSomethingId) {
+		//removes white space
+		$newSomethingId = trim($newSomethingId);
+		//checks if int
+		$newSomethingId = filter_var($newSomethingId, FILTER_SANITIZE_NUMBER_INT);
+		if($newSomethingId === false) {
+			throw(new \InvalidArgumentException('Something Id is not a number.'))
+		}
+		$this->somethingId = $newSomethingId;
+	}
+
+	/**
+	 * Setter for somethingName
+	 *
+	 * @param string $newSomethingName
+	 * @throws /InvalidArgumentException if not a string
+	 */
+	public function setSomethingName($newSomethingName) {
+		//removes white space
+		$newSomethingName = trim($newSomethingName);
+		//checks if string
+		$newSomethingName = filter_var($newSomethingName, FILTER_SANITIZE_STRING);
+		if(empty($newSomethingName)===true) {
+			throw(new \InvalidArgumentException('Something Name is either empty or invalid'));
+		}
+		$this->somethingName = $newSomethingName;
+	}
+	public function exposeSomething($somethingName) {
+		//exposes something as something else merely disguised as something.
+		$this->somethingName = 'somethingDisguisedAs' . $somethingName;
+	}
+
+}
