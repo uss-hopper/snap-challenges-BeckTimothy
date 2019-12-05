edd2a0335957cff9dd836d41580e7e671f9a5efb
Timothy Beck <johndaveleet@gmail.com>
2019-12-05 09:45:21 -0700

    cleaned up extra closing tag in html and added map function to make the list and ul

--- snap-12-5/script.js
+++ snap-12-5/script.js
@@ -15,45 +15,10 @@ const getBreeds = async () => {
 
 const listBreeds = async () => {
 	const breeds = await getBreeds();
-
 	if (breeds.data.message) {
 		let target = document.getElementById('target');
-		let breedList = Object.keys(breeds.data.message);
-		target.innerHTML = Object.keys(breeds.data.message).map(x => `<li>${x}</li>`).toString().replace(/,/g, '');
+		let breedList = Object.keys(breeds.data.message).map(x => `<li>${x}</li>`).toString().replace(/,/g, '');
+		target.innerHTML = breedList;
 		console.log(`Got ${Object.entries(breeds.data.message).length} breeds`)
 	}
 };
-
-// const getBreeds = async () => {
-// 	try {
-// 		console.log('api connection made');
-// 		return await axios.get('https://dog.ceo/api/breeds/list/all');
-// 	} catch (error) {
-// 		console.error(error)
-// 	}
-// };
-// const listBreeds = async () => {
-// 	const breeds = await getBreeds();
-// 	console.log('breeds recieved');
-// 	let target = document.getElementById('target');
-// 	target.innerText = breeds.data.message;
-// 	return Object.keys(breeds.data.message);
-// };
-// const getBreeds = async () => {
-// 	try {
-// 		return await axios.get('https://dog.ceo/api/breeds/list/all')
-// 	} catch (error) {
-// 		console.error(error)
-// 	}
-// };
-//
-// const printBreeds = async () => {
-// 	const breeds = await getBreeds();
-//
-// 	if (breeds.data.message) {
-// 		console.log(`Got ${Object.entries(breeds.data.message).length} breeds`)
-// 		return Object.entries(breeds.data.message);
-// 	}
-// };
-
-onPageLoad();
